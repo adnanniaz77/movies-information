483f570ee3dbcadd6b55e0d95f94aee193eed80c
Adnan Niaz <adnanniaz77@yahoo.com>
2020-08-03 14:08:08 -0700

    initial files

---
+++ src/components/MoviesGrid.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MoviesGrid() {
+    return (
+        <div className="movies-grid">
+            <h2>Movies Grid</h2>
+        </div>
+    );
+}
