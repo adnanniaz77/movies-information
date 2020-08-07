1a5ddb22face32d294055f0182f5e6ccfd599a00
Adnan Niaz <adnanniaz77@yahoo.com>
2020-08-06 18:50:18 -0700

    testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = () => {
     return (
         <div className="header">
-            <h1>Movies Information</h1>
+            <h1>Movies Information Micky</h1>
             <span className="tagline">Browse Movies Information</span>
         </div>
     );
