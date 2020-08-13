cd797052999c0455b19defba152bbfc7c4aa53b2
Adnan Niaz <adnanniaz77@yahoo.com>
2020-08-12 19:36:07 -0700

    general update

--- src/tmdb/useApi.js
+++ src/tmdb/useApi.js
@@ -4,11 +4,11 @@ import axios from "axios";
 const useApi = (query) => {
     const [apiData, setApiData] = useState([]);
     const apiKey = "22e7c1bb96e31278c7328fa9a52b5bad";
-    const urlUpcoming = `https://api.themoviedb.org/3/movie/${query}?language=en-US&api_key=${apiKey}`;
+    const searchUrl = `https://api.themoviedb.org/3/movie/${query}?language=en-US&api_key=${apiKey}`;
 
     useEffect(() => {
         const fetchMovies = async () => {
-            const result = await axios.get(urlUpcoming);
+            const result = await axios.get(searchUrl);
             setApiData(result.data.results);
         };
 
